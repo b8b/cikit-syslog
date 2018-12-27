ba54160d86fc8e11a6b15a503ff6456178a88c54
b8b <25864173+b8b@users.noreply.github.com>
2018-12-27 12:23:19 +0100

    add bintray gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("jvm") version kotlinVersion
     `maven-publish`
     id("me.champeau.gradle.jmh") version "0.4.7"
+    id("com.jfrog.bintray") version "1.8.4"
 }
 
 group = "org.cikit.syslog"
@@ -58,6 +59,26 @@ publishing {
         register("mavenJava", MavenPublication::class) {
             from(components["java"])
             artifact(sourcesJar.get())
+        }
+    }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_KEY")
+    setPublications("mavenJava")
+    pkg.apply {
+        repo = "cikit"
+        name = "cikit-syslog"
+        userOrg = user
+        setLicenses("Apache-2.0")
+        vcsUrl = "https://github.com/b8b/cikit-syslog.git"
+        setLabels("kotlin")
+        publicDownloadNumbers = true
+        version.apply {
+            name = project.version.toString()
+            desc = "rfc5424 syslog implementation"
+            vcsTag = "v${project.version}"
         }
     }
 }
